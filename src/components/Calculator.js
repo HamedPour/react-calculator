e0dec0780307b431ad0fecb7c42e3f4b18305c92
HamedPour <hamed_k.pour@hotmail.com>
2020-05-27 16:36:23 +0100

    Setting up states and functions

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,30 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./componentStyles/global.css";
 import "./componentStyles/grid.css";
 
 const Calculator = () => {
+  const [display, setDisplay] = useState("0");
+
+  // Need a function "updateDisplay" that adds all the digits
+  // into display when the number keys are pressed
+
+  // Need a function to deal with operations - something that records
+  // the state of display when an operation key is pressed and then uses
+  // that operation to perform calculations on the second display state which
+  // is added afterwards.
+
   return (
     <>
       <div className="calWrapper">
-        <div className="btn display">display</div>
-        <div className="btn clearBtn">C</div>
+        <div className="btn display">{display}</div>
+        <div className="btn clearBtn clickAble">C</div>
         <div className="btn percentBtn">%</div>
-        <div className="btn divisonBtn">&#247;</div>
-        <div className="btn sevenBtn">7</div>
-        <div className="btn eightBtn">8</div>
-        <div className="btn nineBtn">9</div>
-        <div className="btn multiplyBtn">X</div>
-        <div className="btn fourBtn">4</div>
-        <div className="btn fiveBtn">5</div>
-        <div className="btn sixBtn">6</div>
-        <div className="btn subtractBtn">-</div>
-        <div className="btn oneBtn">1</div>
-        <div className="btn twoBtn">2</div>
-        <div className="btn threeBtn">3</div>
-        <div className="btn additionBtn">+</div>
-        <div className="btn zeroBtn">0</div>
-        <div className="btn dotBtn">.</div>
-        <div className="btn equalBtn">=</div>
+        <div className="btn divisonBtn clickAble">&#247;</div>
+        <div
+          className="btn sevenBtn clickAble"
+          onClick={() => {
+            setDisplay(display + "7");
+          }}
+        >
+          7
+        </div>
+        <div className="btn eightBtn clickAble">8</div>
+        <div className="btn nineBtn clickAble">9</div>
+        <div className="btn multiplyBtn clickAble">x</div>
+        <div className="btn fourBtn clickAble">4</div>
+        <div className="btn fiveBtn clickAble">5</div>
+        <div className="btn sixBtn clickAble">6</div>
+        <div className="btn subtractBtn clickAble">&#8722;</div>
+        <div className="btn oneBtn clickAble">1</div>
+        <div className="btn twoBtn clickAble">2</div>
+        <div className="btn threeBtn clickAble">3</div>
+        <div className="btn additionBtn clickAble">+</div>
+        <div className="btn zeroBtn clickAble">0</div>
+        <div className="btn dotBtn clickAble">.</div>
+        <div className="btn equalBtn clickAble">=</div>
       </div>
     </>
   );
